932e2e7e8eac6794e71df4ab894e66ea38b7c6ca
Cole Joko <joko.cole.cj@gmail.com>
2020-08-16 15:34:23 -0600

    added connection.js file. added controllers and config folders

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    port 6000,
+    user: 'root',
+    password: "",
+    database: 'burger_db'
+  });
+
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
